8789bf49cc08f3b06e59eab4b0a669667fa124cd
kaushal234 <77381151+kaushal234@users.noreply.github.com>
2026-07-18 12:23:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import time
+from datetime import datetime
 
-print("=" * 50)
-print("Running Python Application")
-time.sleep(5)
-print("Application Finished")
+print("=" * 60)
+print("Application Execution Started")
+print(f"Time : {datetime.now()}")
+
+for i in range(1, 6):
+    print(f"Processing Task {i}")
+    time.sleep(1)
+
+print("Application Completed Successfully")
+print("=" * 60)
